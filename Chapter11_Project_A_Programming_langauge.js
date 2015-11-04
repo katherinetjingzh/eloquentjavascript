6388de58e95e093083e71e034569a87e79dcb450
Jing <tjingzh@gmail.com>
2015-11-04 07:39:20 -0800

    Finish Chapter 11

--- Chapter11_Project_A_Programming_langauge.js
+++ Chapter11_Project_A_Programming_langauge.js
@@ -38,9 +38,11 @@ function parseExpression(program) {
 }
 
 function skipSpace(string) {
-  var first = string.search(/\S/);
-  if (first == -1) return "";
-  return string.slice(first);
+  // var first = string.search(/\S/);
+  // if (first == -1) return "";
+  // return string.slice(first);
+  var skippable = string.match(/^(\s|#.*)*/);
+  return string.slice(skippable[0].length);
 }
 
 function parseApply(expr, program) {
@@ -166,6 +168,135 @@ topEnv["print"] = function(value) {
   console.log(value);
   return value;
 };
+
+specialForms["fun"] = function(args, env) {
+  if (!args.length)
+    throw new SyntaxError("Functions need a body");
+  function name(expr) {
+    if (expr.type != "word")
+      throw new SyntaxError("Arg names must be words");
+    return expr.name;
+  }
+  var argNames = args.slice(0, args.length - 1).map(name);
+  var body = args[args.length - 1];
+
+  return function() {
+    if (arguments.length != argNames.length)
+      throw new TypeError("Wrong number of arguments");
+    var localEnv = Object.create(env);
+    for (var i = 0; i < arguments.length; i++)
+      localEnv[argNames[i]] = arguments[i];
+    return evaluate(body, localEnv);
+  };
+};
+
+
+function run() {
+  var env = Object.create(topEnv);
+  var program = Array.prototype.slice
+    .call(arguments, 0).join("\n");
+  return evaluate(parse(program), env);
+}
+
+run("do(define(plusOne, fun(a, +(a, 1))),",
+    "   print(plusOne(10)))");
+// → 11
+
+run("do(define(pow, fun(base, exp,",
+    "     if(==(exp, 0),",
+    "        1,",
+    "        *(base, pow(base, -(exp, 1)))))),",
+    "   print(pow(2, 10)))");
+// → 1024
+
+// Or imagine you are building a giant robotic dinosaur and need to program its
+// behavior. JavaScript might not be the most effective way to do this. You
+// might instead opt for a language that looks like this:
+
+// behavior walk
+//   perform when
+//     destination ahead
+//   actions
+//     move left-foot
+//     move right-foot
+
+// behavior attack
+//   perform when
+//     Godzilla in-view
+//   actions
+//     fire laser-eyes
+//     launch arm-rockets
+// This is what is usually called a domain-specific language, a language
+// tailored to express a narrow domain of knowledge. Such a language can be more
+// expressive than a general-purpose language because it is designed to express
+// exactly the things that need expressing in its domain and nothing else.
+
+
+// Exercise 
+// Exercise 1:
+// Array.prototype.slice can be used to convert an arguments array-like object
+// into a regular array.
+topEnv["array"] = function() {
+	return Array.prototype.slice.call(arguments, 0);
+};
+
+topEnv["length"] = function(array) {
+	array.length;
+};
+
+topEnv["element"] = function(array, i) {
+	return array[i];
+};
+
+run("do(define(sum, fun(array,",
+    "     do(define(i, 0),",
+    "        define(sum, 0),",
+    "        while(<(i, length(array)),",
+    "          do(define(sum, +(sum, element(array, i))),",
+    "             define(i, +(i, 1)))),",
+    "        sum))),",
+    "   print(sum(array(1, 2, 3))))");
+// → 6
+
+
+// Exercise 3: Comment.
+console.log(parse("# hello\nx"));
+// → {type: "word", name: "x"}
+
+console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "x"},
+//    args: []}
+
+// Exercise 4 : Fixing Scope.
+// Scopes do not derive from Object.prototype, so if you want to call
+// hasOwnProperty on them, you have to use this clumsy expression.
+specialForms["set"] = function(args, env) {
+  // Your code here.
+  if (args.length != 2 || args[0].type != "word") {
+  	throw new SyntaxError("Bad use of set");
+  }
+  var varName = args[0].name;
+  var value = evaluate(args[1], env);
+
+  for (var scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+  	if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+  		scope[varName] = value;
+  		return value;
+  	}
+  }
+  throw new ReferenceError("Setting undefined variable " + varName);
+};
+
+run("do(define(x, 4),",
+    "   define(setx, fun(val, set(x, val))),",
+    "   setx(50),",
+    "   print(x))");
+// → 50
+run("set(quux, true)");
+// → Some kind of ReferenceError
+
+
 
 
 
